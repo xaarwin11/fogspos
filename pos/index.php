b1ed00a922ff7960370691ce14b295473924e4b2
xaarwin11 <predsgamer@gmail.com>
2026-03-28 23:18:30 +0800

    Fixed inconsistent file name

--- pos/index.php
+++ pos/index.php
@@ -10,7 +10,7 @@ if (empty($_SESSION['user_id'])) { header("Location: ../"); exit; }
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include 'pwa_universal.php'; ?>
+    <?php include 'pwa.php'; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>POS - FogsTasa</title>
